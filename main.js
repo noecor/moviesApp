bfcfcf0ecd8ca7ddc09fb65182d4171536c2f10d
noecor <noeliacoronelg@hotmail.com>
2019-08-19 20:09:17 -0300

    imprimo datos en consola

--- main.js
+++ main.js
@@ -1,37 +1,87 @@
 console.log('Hola');
 const apiKey = `3631da5fba4115a4ce320e971a2fdadb`;
 const paginaActual = 1;
+//const peliculaId = e;
+//const searchText = ;
 
 fetch("https://api.themoviedb.org/3/movie/550?api_key=3631da5fba4115a4ce320e971a2fdadb")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+// categoria Popular
+const popular = fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+
+// categoría Top Rated
+const topRated = fetch (`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+    })
+// categoría Upcoming
+const upcoming = fetch (`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+    })
+  
+// categoría Now Playing
+const nowPlaying = fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`)
     .then(response => response.json())
     .then(res => {
         console.log(res);
     })
 
-// categoria Popular
-`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
-// categoría Top Rated
-`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
-// categoría Upcoming
-`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`
-// categoría Now Playing
-`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`
 
+
+//Listado por categoría o búsqueda
 // si hicimos click en la categoria Popular, las peliculas a mostrar la sacamos de:
-`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${paginaActual}`
+const popularList = fetch (`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&page=${paginaActual}`)
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+    })
 
 // si hicimos click en la categoria Top Rated, las peliculas a mostrar la sacamos de:
-`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=${paginaActual}`
+const topList = fetch (`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=${paginaActual}`)
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+    })
 
 // si hicimos click en la categoria Upcoming, las peliculas a mostrar la sacamos de:
-`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&page=${paginaActual}`
+const upcomingList = fetch (`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}&page=${paginaActual}`)
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+    })
 
 // si hicimos click en la categoria Now Playing, las peliculas a mostrar la sacamos de:
-`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${paginaActual}`
+const nowPlayingList = fetch (`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&page=${paginaActual}`)
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+    })
 
-// si hicimos una búsqueda por palabra clave, las peliculas a mostrar la sacamos de:
-`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${textoBusqueda}&page=${paginaActual}`
+/*/ si hicimos una búsqueda por palabra clave, las peliculas a mostrar la sacamos de:
+DEFINIR VARIBLE searchText
+const search = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchText}&page=${paginaActual}`)
+    .then(response => response.json())
+    .then(res => {
+        console.log(res);
+    })
+    
+*/
 
 //Modal
-
-`https://api.themoviedb.org/3/movie/${peliculaId}?api_key=${apiKey}`
+/*const movieData = fetch (`https://api.themoviedb.org/3/movie/${peliculaId}?api_key=${apiKey}`)
+.then(response => response.json())
+.then(res => {
+    console.log(res);
+})
+console.log (movieData)*/
